89e5f7b79f91b4e39ac2d72ef6915bc13bbf602c
user2 <user2@KH_C>
2019-02-08 17:59:32 +0900

    가로세로 수정

--- src/com/kh/game/model/view/GamePanel.java
+++ src/com/kh/game/model/view/GamePanel.java
@@ -68,9 +68,8 @@ public class GamePanel extends JPanel{
 			}
 		});
 
-		JTextArea munjae = new JTextArea();
-
-		munjae.append("<가로문제>\n\n" + 
+		//스크롤바 최상단 위치하기 위해서 append를 쓰지않고 바로 내용을 넣어줌..
+		JTextArea munjae = new JTextArea("<가로문제>\n\n" + 
 				"1. Primitive Data Type을 객체화해주는 클래스는?\n\n" + 
 				"2. 어떤 변수가 값을 가지지 않음을 나타내는 특별한 값은?\n\n" + 
 				"3. 반복문을 빠져나갈 때 사용하는 구문은?\n\n" + 
@@ -84,6 +83,21 @@ public class GamePanel extends JPanel{
 				"4. byte * short 연산 시 결과값의 자료형은?\n\n" + 
 				"5. 접근제한자 중 범위가 가장 좁은 것?\n\n" + 
 				"6. 출력문에서 데이터를 출력하고 자동으로 줄바꿈 처리를 해주는 메소드는?");
+
+		/*munjae.append("<가로문제>\n\n" + 
+				"1. Primitive Data Type을 객체화해주는 클래스는?\n\n" + 
+				"2. 어떤 변수가 값을 가지지 않음을 나타내는 특별한 값은?\n\n" + 
+				"3. 반복문을 빠져나갈 때 사용하는 구문은?\n\n" + 
+				"4. 조건식의 결과값이 true일 때 블록 안의 내용을 실행하고 false일 때는 실행하지 않는 제어문은? \n\n" + 
+				"5. Date 클래스의 gettime 메소드 사용 시 return 받는 값의 자료형은?\n\n" + 
+				"6. 배열의 길이를 확인할 수 있는 구문은?\n\n\n" + 
+				"<세로문제>\n\n" + 
+				"1. heap 메모리에 객체를 생성하고 stack에 주소값을 넣어주는 연산자는?\n\n" + 
+				"2. 자신을 호출한 메소드로 돌아가는 구문은?\n\n" + 
+				"3. 접근제한자 중 어디에서든 접근이 가능한 것은?\n\n" + 
+				"4. byte * short 연산 시 결과값의 자료형은?\n\n" + 
+				"5. 접근제한자 중 범위가 가장 좁은 것?\n\n" + 
+				"6. 출력문에서 데이터를 출력하고 자동으로 줄바꿈 처리를 해주는 메소드는?");*/
 
 		munjae.setLineWrap(true);
 		munjae.setFont(new Font("굴림체", Font.PLAIN, 16));
