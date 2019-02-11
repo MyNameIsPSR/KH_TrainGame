4ce8674906ad03b34a9f50d7e7403fd814dcab4b
Jody <Jody@DESKTOP-O8F1PHG>
2019-02-11 20:43:11 +0900

    가로세로 기능 추가

--- src/com/kh/game/model/view/CrossPanel.java
+++ src/com/kh/game/model/view/CrossPanel.java
@@ -1,30 +1,19 @@
 package com.kh.game.model.view;
 
-import java.awt.Color;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
 
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
 
-import com.kh.game.model.talk.Talk3;
-
-//가로세로낱말퍼즐
-public class CrossPanel extends JPanel implements ActionListener{
-	private MainFrame mf; 
-	private JButton btnNext = new JButton();
+public class CrossPanel extends JPanel{
+	private MainFrame mf;
 	
-	private JPanel SubPanel;
+	private JPanel CrossPanel;
 
 	public CrossPanel(MainFrame mf) {
 		this.mf = mf;
-		this.SubPanel = this;
+		this.CrossPanel = this;
 
 		this.setSize(900, 600);
-		this.setLocation(0, 0);
 		this.setBackground(Color.gray);
 		this.setLayout(null);
 
@@ -34,22 +23,9 @@ public class CrossPanel extends JPanel implements ActionListener{
 		crossword.setSize(500, 40);
 		crossword.setLocation(200, 30);
 
+		new GamePanel(this, new GameBoard());
 
 		this.add(crossword);
 		mf.add(this);
-		new GamePanel(this);
 	}
-	
-	public void setBtnNext(JButton btnNext) { //GamePanel로부터 next버튼을 받아옴
-		this.btnNext = btnNext;
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == btnNext) {
-			//성공과 실패시 대화 화면으로 넘어가기(수정필요)
-			ChangePanel.changePanel(mf, SubPanel, 
-					new Talk3(mf));
-		}
-	} 
 }
